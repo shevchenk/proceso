cad1b2ba6e37a87e66d4e4cc4952a2e2d1b72e5b
shevchenk <jorgeshevchenk@gmail.com>
2019-05-14 17:53:08 -0500

    act

--- laravel/app/views/admin/reporte/bandejatramitedig.blade.php
+++ laravel/app/views/admin/reporte/bandejatramitedig.blade.php
@@ -126,10 +126,10 @@ table>thead>tr>td,table>tfoot>tr>td{
 </style>
             <!-- Content Header (Page header) -->
     <section class="content-header">
-        <h1>
+        <h2>
             Bandeja de Activades pendientes
             <small> </small>
-        </h1>
+        </h2>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Admin</a></li>
             <li><a href="#">Reporte</a></li>
