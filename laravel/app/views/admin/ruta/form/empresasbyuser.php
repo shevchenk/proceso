86c5d70d1f2c99c6f601170b115b7ca53cbbd013
shevchenk <jorgeshevchenk@gmail.com>
2020-08-14 11:31:05 -0500

    act

--- laravel/app/views/admin/ruta/form/empresasbyuser.php
+++ laravel/app/views/admin/ruta/form/empresasbyuser.php
@@ -5,7 +5,7 @@
     <div class="modal-content">
       <div class="modal-header logo">
          <button type="button" class="close btn-xs" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title">Buscar Tramite</h4>
+        <h4 class="modal-title">Buscar Persona Jurídica</h4>
       </div>
       <div class="modal-body">
             <div class="box-body table-responsive">
